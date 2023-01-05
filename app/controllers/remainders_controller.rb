0ab1611b3705392e0f15fcfe0e9309d547e2aeb2
TAMAKI <a2226tt@aiit.ac.jp>
2023-01-05 23:33:08 +0900

    1月5日

--- app/controllers/remainders_controller.rb
+++ app/controllers/remainders_controller.rb
@@ -1,28 +1,24 @@
 class RemaindersController < ApplicationController
-      # GET /Remainders or /Remainders.json
+
   def index
     @remainders = RemaindItem.all
   end
-  # GET /users/1 or /users/1.json
+
   def show
   end
 
-  # GET /users/new
   def new
     @remaind_item = RemaindItem.new
     @remaind = Remaind.new
   end
 
-  # GET /users/1/edit
   def edit
   end
 
   def create
-  # p params
-    p remaind_item_params
     remaind_item=RemaindItem.new(remaind_item_params)
+    remaind_item.user_id=1
     remaind_item.remainds.build(remaind_params)
-
     respond_to do |format|
       if remaind_item.save
         format.html { redirect_to remainders_url, notice: "remainders was successfully created." }
@@ -41,11 +37,11 @@ class RemaindersController < ApplicationController
   end
 
   private
-    # Only allow a list of trusted parameters through.
-  def remaind_item_params
-      params.require(:remaind_item).permit(:description)
-  end
-  def remaind_params
-    params.require(:remaind).permit(:remaind_at)
-end 
+    def remaind_item_params
+        params.require(:remaind_item).permit(:description)
+    end
+    
+    def remaind_params
+      params.require(:remaind).permit(:remaind_at)
+    end 
 end
